33f7d7ccabf9ce97f40f5f6bdb6cf8fc40140c1f
thyjukki <judejoki@gmail.com>
2015-02-04 16:01:34 +0200

    Added cvars

---
+++ gradian/cvar.hpp
@@ -0,0 +1,97 @@
+/*******************************************************************
+** This code is part of Gradian game engine
+**
+** All code is free to use if proper credit is given.
+** If you found this code without proper credit please contact me
+** Jussi Joki (judejoki@gmail.com)
+******************************************************************/
+
+// Cvars work similiar how valve source engine and id1 quake engines work.
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+using namespace std;
+extern void debugPrint(string message);
+extern void errorPrint(string message);
+
+enum cvarFlag{
+	NONE = (1 << 0),
+	SAVE = (1 << 2),
+};
+
+
+class Cvar
+{
+	string name = "";
+	string description = "";
+	string value = "";
+	string baseValue = "";
+	cvarFlag flags = NONE;
+public:
+	Cvar(string a, string b) : name(a), description(b)
+	{
+	}
+	Cvar(string a, string b, int value) : name(a), description(b)
+	{
+		this->value = to_string(value);
+	}
+	Cvar(string a, string b, float value) : name(a), description(b)
+	{
+		this->value = to_string(value);
+	}
+	Cvar(string a, string b, string c) : name(a), description(b), value(c)
+	{
+	}
+
+	Cvar(string a, string b, cvarFlag d) : name(a), description(b), flags(d)
+	{
+	}
+	Cvar(string a, string b, int value, cvarFlag d) : name(a), description(b), flags(d)
+	{
+		this->value = to_string(value);
+	}
+	Cvar(string a, string b, float value, cvarFlag d) : name(a), description(b), flags(d)
+	{
+		this->value = to_string(value);
+	}
+	Cvar(string a, string b, string c, cvarFlag d) : name(a), description(b), value(c), flags(d)
+	{
+	}
+
+	void setupCvar();
+
+	Cvar& operator=(string value)
+	{
+		this->value = value;
+
+		return *this;
+	}
+
+	Cvar& operator=(float value)
+	{
+		this->value = to_string(value);
+
+		return *this;
+	}
+
+	Cvar& operator=(int value)
+	{
+		this->value = to_string(value);
+
+		return *this;
+	}
+
+	int toInt()
+	{
+		return atoi(this->value.c_str());
+	}
+
+	float toFloat()
+	{
+		return (float)atof(this->value.c_str());
+	}
+};
+extern std::map<std::string, Cvar *> cvarList;
